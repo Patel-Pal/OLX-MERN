7f0c39b973e522d9efe28dde272d3aef38aadf70
Patel-pal <patelpal.93130@gmail.com>
2025-07-29 14:17:57 +0530

    vitconfig and vercel.json

--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: true, // Enable HTTPS
-  },
-  build: {
-    chunkSizeWarningLimit: 1500 // Increase from default 500 KB to 1.5 MB
-  }
+  // server: {
+  //   https: true, // Enable HTTPS
+  // },
+  // build: {
+  //   chunkSizeWarningLimit: 1500 // Increase from default 500 KB to 1.5 MB
+  // }
 })
